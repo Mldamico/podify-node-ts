de60c1cb75f281fc0c3631ce9a09437c35a3c3c0
mldamico <matias.lioneldamico@gmail.com>
2023-09-24 22:20:14 -0300

    Validator middleware

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,10 +1,12 @@
+import { validate } from '@/middleware/validator';
 import User from '@/models/user';
 import { CreateUser } from '@/types/user';
+import { CreateUserSchema } from '@/utils/validationSchema';
 import { Router } from 'express'
 
 const router = Router();
 
-router.post("/signup", async (req: CreateUser, res) => {
+router.post("/signup", validate(CreateUserSchema), async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
 
   const user = await User.create({ name, email, password })
