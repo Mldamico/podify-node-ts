d59cfd20230c42446a7df52efa87b0d3c1e8b8ad
mldamico <matias.lioneldamico@gmail.com>
2023-09-24 21:42:17 -0300

    CreateUser type

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,9 +1,10 @@
 import User from '@/models/user';
+import { CreateUser } from '@/types/user';
 import { Router } from 'express'
 
 const router = Router();
 
-router.post("/signup", async (req, res) => {
+router.post("/signup", async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
 
   const user = await User.create({ name, email, password })
