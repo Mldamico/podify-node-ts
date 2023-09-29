283a064ee6a0e1bc1492d2b82e8256ebed30a7ed
mldamico <matias.lioneldamico@gmail.com>
2023-09-29 17:35:41 -0300

    email token number

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,12 +1,17 @@
 import User from "@/models/user";
+import EmailVerificationToken from "@/models/emailVerificationToken";
 import { CreateUser } from "@/types/user";
+import { generateToken } from "@/utils/helper";
 import { MAILTRAP_PASS, MAILTRAP_USER } from "@/utils/variables";
 import { Response } from "express";
 import nodemailer from 'nodemailer';
+
 export const create = async (req: CreateUser, res: Response) => {
   const { email, password, name } = req.body;
 
   const user = await User.create({ name, email, password });
+
+
 
   const transport = nodemailer.createTransport({
     host: "sandbox.smtp.mailtrap.io",
@@ -17,10 +22,17 @@ export const create = async (req: CreateUser, res: Response) => {
     }
   });
 
+  const token = generateToken(6);
+  await EmailVerificationToken.create({
+    owner: user._id,
+    token
+  });
+
+
   transport.sendMail({
     to: user.email,
     from: "auth@fake.com",
-    html: '<h1></h1>'
+    html: `<h1>Your verification token is ${token}</h1>`
   });
   res.status(201).json({ user });
 };
