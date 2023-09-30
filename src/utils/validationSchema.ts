d3df70ba1568da875eec685a296eb598c26a06ab
mldamico <matias.lioneldamico@gmail.com>
2023-09-30 17:49:30 -0300

    Email verification body middleware

--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -1,4 +1,6 @@
 import * as yup from 'yup';
+import { isValidObjectId } from 'mongoose';
+
 export const CreateUserSchema = yup.object().shape({
   name: yup.string().trim().required("Name is required!").min(3, "Name is too short!").max(20, 'Name is too long!'),
   email: yup.string().required("Email is required").email("Invalid email"),
@@ -7,5 +9,15 @@ export const CreateUserSchema = yup.object().shape({
       /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])[a-zA-Z\d!@#\$%\^&\*]+$/,
       "Password is too simple!"
     ),
-})
+});
 
+export const EmailVerificationBody = yup.object().shape({
+  token: yup.string().trim().required("Invalid token"),
+  userId: yup.string().transform(function (value) {
+    if (this.isType(value) && isValidObjectId(value)) {
+      return value;
+    }
+    return "";
+
+  }).required("Invalid userId")
+});
