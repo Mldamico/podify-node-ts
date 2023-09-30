488098cdef79c57f594e2bb70e1b95ef9dc6f489
mldamico <matias.lioneldamico@gmail.com>
2023-09-30 16:57:19 -0300

    Mail util for generating token and template

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,12 +1,10 @@
 import User from "@/models/user";
-import EmailVerificationToken from "@/models/emailVerificationToken";
 import { CreateUser } from "@/types/user";
 import { generateToken } from "@/utils/helper";
-import { MAILTRAP_PASS, MAILTRAP_USER } from "@/utils/variables";
+import { sendVerificationMail } from "@/utils/mail";
+
 import { Response } from "express";
-import nodemailer from 'nodemailer';
-import { generateTemplate } from "@/mail/template";
-import path from 'path';
+
 
 export const create = async (req: CreateUser, res: Response) => {
   const { email, password, name } = req.body;
@@ -14,47 +12,8 @@ export const create = async (req: CreateUser, res: Response) => {
 
   const user = await User.create({ name, email, password });
 
+  const token = generateToken();
+  sendVerificationMail(token, { name, email, userId: user._id.toString() });
 
-
-  const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: MAILTRAP_USER,
-      pass: MAILTRAP_PASS
-    }
-  });
-
-  const token = generateToken(6);
-  await EmailVerificationToken.create({
-    owner: user._id,
-    token
-  });
-
-  transport.sendMail({
-    to: user.email,
-    from: "auth@fake.com",
-    subject: 'Welcome to Podify',
-    html: generateTemplate({
-      title: "Welcome to Podify",
-      message: `Hi ${name}, welcome to Podify! There are so much thing that we do for verified users. Use the given OTP to verify your email.`,
-      logo: "cid:logo",
-      banner: "cid:welcome",
-      link: "#",
-      btnTitle: token
-    }),
-    attachments: [
-      {
-        filename: "logo.png",
-        path: path.join(__dirname, "../mail/logo.png"),
-        cid: "logo"
-      },
-      {
-        filename: "welcome.png",
-        path: path.join(__dirname, "../mail/welcome.png"),
-        cid: "welcome"
-      },
-    ]
-  });
-  res.status(201).json({ user });
+  res.status(201).json({ user: { id: user._id, name, email } });
 };
--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -1,8 +1,11 @@
-import * as yup from 'yup'
+import * as yup from 'yup';
 export const CreateUserSchema = yup.object().shape({
   name: yup.string().trim().required("Name is required!").min(3, "Name is too short!").max(20, 'Name is too long!'),
   email: yup.string().required("Email is required").email("Invalid email"),
   password: yup.string().trim().required("Password is required").min(6, "Password is too short")
-    .matches(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])[a-zA-Z\d!@#\$%\^&\*]+$/, "Password is too simple")
+    .matches(
+      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#\$%\^&\*])[a-zA-Z\d!@#\$%\^&\*]+$/,
+      "Password is too simple!"
+    ),
 })
 
