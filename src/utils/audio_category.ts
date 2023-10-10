38812a861ed71be4a9ee4a167ab612743b0735ff
mldamico <matias.lioneldamico@gmail.com>
2023-10-09 22:07:55 -0300

    Audio model

---
+++ src/utils/audio_category.ts
@@ -0,0 +1,22 @@
+export const categories = [
+  'Arts',
+  'Bussiness',
+  'Education',
+  'Entertainment',
+  'Kids & Family',
+  'Music',
+  'Science',
+  'Tech',
+  'Others'
+];
+
+export type categoriesTypes =
+  | 'Arts'
+  | 'Bussiness'
+  | 'Education'
+  | 'Entertainment'
+  | 'Kids & Family'
+  | 'Music'
+  | 'Science'
+  | 'Tech'
+  | 'Others';
